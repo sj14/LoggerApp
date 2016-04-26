e9eaeeade9a4255dfb3acdf5c5a9bce37b101572
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2016-04-26 12:31:21 +0200

    Using PreferenceFragment instead deprecated method.

--- app/src/main/java/org/hsrw/mobilecomputing/loggerapp/SettingsActivity.java
+++ app/src/main/java/org/hsrw/mobilecomputing/loggerapp/SettingsActivity.java
@@ -1,13 +1,28 @@
 package org.hsrw.mobilecomputing.loggerapp;
 
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SettingsActivity extends PreferenceActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
