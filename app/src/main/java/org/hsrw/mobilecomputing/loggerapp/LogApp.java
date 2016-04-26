5e00d133f4e0f23b2a7e7dd408e2ca5ba4e0c276
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2016-04-26 12:48:48 +0200

    Basic LogElement classes.

---
+++ app/src/main/java/org/hsrw/mobilecomputing/loggerapp/LogApp.java
@@ -0,0 +1,14 @@
+package org.hsrw.mobilecomputing.loggerapp;
+
+import java.util.Date;
+
+/**
+ * Created by simon on 26.04.16.
+ * Represents a log of an used App.
+ */
+public class LogApp extends LogElement {
+
+    public LogApp(Date date, String name) {
+        super(date, name);
+    }
+}
---
+++ app/src/main/java/org/hsrw/mobilecomputing/loggerapp/LogCall.java
@@ -0,0 +1,14 @@
+package org.hsrw.mobilecomputing.loggerapp;
+
+import java.util.Date;
+
+/**
+ * Created by simon on 26.04.16.
+ * Represents a log of a call.
+ */
+public class LogCall extends LogElement {
+
+    public LogCall(Date date, String name) {
+        super(date, name);
+    }
+}
