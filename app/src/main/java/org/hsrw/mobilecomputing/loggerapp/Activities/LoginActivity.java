2174220e1933a4310377557347d9e3e3e5507d83
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2016-05-05 14:03:52 +0200

    Login: Keyboard Done button works like expected

--- app/src/main/java/org/hsrw/mobilecomputing/loggerapp/Activities/LoginActivity.java
+++ app/src/main/java/org/hsrw/mobilecomputing/loggerapp/Activities/LoginActivity.java
@@ -1,9 +1,14 @@
 package org.hsrw.mobilecomputing.loggerapp.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,24 +20,55 @@ public class LoginActivity extends AppCompatActivity {
     Button mLoginButton;
     TextView mWrontPasswordText;
     EditText mPassword;
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        initialiseElements();
+        passwordCheckEnabled();
+    }
+
+    private void initialiseElements() {
         mLoginButton = (Button) findViewById(R.id.btn_open);
         mWrontPasswordText = (TextView) findViewById(R.id.textView_wrongPassword);
         mPassword = (EditText) findViewById(R.id.editText_password);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        mPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    onClickLogin();
+                }
+                return false;
+            }
+        });
+
     }
 
-    public void onClickLogin(View view) {
-        Intent intent = new Intent(this, LogActivity.class);
+    private void passwordCheckEnabled() {
+        boolean prefEnablePassword = sharedPref.getBoolean("pref_enable_password", true);
 
-        if(mPassword.getText().toString().equals("0000")) {
+        if (!prefEnablePassword) {
+            Intent logActivity = new Intent(this, LogActivity.class);
+            startActivity(logActivity);
+        }
+    }
+
+    public void onClickLogin() {
+        String prefPassword = sharedPref.getString("pref_password", "");
+
+
+        if(mPassword.getText().toString().equals(prefPassword)) {
+            Intent intent = new Intent(this, LogActivity.class);
             startActivity(intent);
         } else {
             mWrontPasswordText.setVisibility(View.VISIBLE);
         }
     }
+
+
+
 }
