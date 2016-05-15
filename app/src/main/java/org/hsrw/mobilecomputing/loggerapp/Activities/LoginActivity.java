fa84c9df8473f47d1ca25d9230beccfd9ce2b40c
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2016-05-15 13:15:42 +0200

    request run-time permissions

--- app/src/main/java/org/hsrw/mobilecomputing/loggerapp/Activities/LoginActivity.java
+++ app/src/main/java/org/hsrw/mobilecomputing/loggerapp/Activities/LoginActivity.java
@@ -1,11 +1,14 @@
 package org.hsrw.mobilecomputing.loggerapp.activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -17,6 +20,8 @@ import org.hsrw.mobilecomputing.loggerapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST = 0;
+
     Button mLoginButton;
     TextView mWrontPasswordText;
     EditText mPassword;
@@ -26,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        checkPermissions();
 
         initialiseElements();
         passwordCheckEnabled();
@@ -73,6 +80,20 @@ public class LoginActivity extends AppCompatActivity {
         onClickLogin();
     }
 
+    public void checkPermissions() {
+
+        // READ PHONE STATE
+        if (ContextCompat.checkSelfPermission(this,
+        Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
 
 
+                ActivityCompat.requestPermissions(this,
+                        new String[]{
+                                Manifest.permission.READ_PHONE_STATE,
+                                Manifest.permission.RECORD_AUDIO,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST);
+        }
     }
+}
