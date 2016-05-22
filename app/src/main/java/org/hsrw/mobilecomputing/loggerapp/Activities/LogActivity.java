16fcd1863b81f55fec87240face2ab530955f834
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2016-05-22 10:55:21 +0200

    Preparation for usage statistics

--- app/src/main/java/org/hsrw/mobilecomputing/loggerapp/Activities/LogActivity.java
+++ app/src/main/java/org/hsrw/mobilecomputing/loggerapp/Activities/LogActivity.java
@@ -1,59 +1,49 @@
 package org.hsrw.mobilecomputing.loggerapp.activities;
 
-import android.app.Activity;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.hsrw.mobilecomputing.loggerapp.LogElement;
 import org.hsrw.mobilecomputing.loggerapp.LogElementAdapter;
 import org.hsrw.mobilecomputing.loggerapp.R;
 import org.hsrw.mobilecomputing.loggerapp.logcall.LogCallElement;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
 import java.util.List;
 
-import static org.hsrw.mobilecomputing.loggerapp.R.id.listView_logs;
+import static org.hsrw.mobilecomputing.loggerapp.R.id.listView_calls;
 
 public class LogActivity extends AppCompatActivity {
 
-    private ListView listViewLogs;
+    private ListView listViewCalls;
+    private ListView listViewUsage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
-        loadItems();
+        loadCallItems();
+        loadUsageItems();
     }
 
-    private void loadItems() {
+    private void loadCallItems() {
         List<LogCallElement> listLogCallElements = LogCallElement.getCallElements(this.getApplication());
 
         if (listLogCallElements != null) {
             final LogElement myListElement_data[] = listLogCallElements.toArray(new LogCallElement[listLogCallElements.size()]);
-            LogElementAdapter adapter = new LogElementAdapter(this, R.layout.listview_item_row, myListElement_data);
-            listViewLogs = (ListView) findViewById(listView_logs);
+            LogElementAdapter adapter = new LogElementAdapter(this, R.layout.listview_call_row, myListElement_data);
+            listViewCalls = (ListView) findViewById(listView_calls);
 
+            if (listViewCalls != null) {
+                listViewCalls.setAdapter(adapter);
 
-            if (listViewLogs != null) {
-                listViewLogs.setAdapter(adapter);
-
-                listViewLogs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                listViewCalls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         Intent i= new Intent(LogActivity.this, RecordActivity.class);
@@ -67,7 +57,13 @@ public class LogActivity extends AppCompatActivity {
         }
     }
 
-    public void onClickSettings(View view) {
+    private void loadUsageItems() {
+        UsageStatsManager mUsageStatsManager = (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+
+    }
+
+
+        public void onClickSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
